df14e8dd3295b44e6cb10de9c1bd7ca61b19e126
Nick Murdoch <nick@nivan.net>
2016-10-20 20:44:51 +0100

    Factor out collision between characters to avoid spritecollide on all characters for every character.

--- novel-evolve/group.py
+++ novel-evolve/group.py
@@ -1,4 +1,5 @@
-from pygame.sprite import Group as pygame_Group
+from itertools import combinations
+from pygame.sprite import Group as pygame_Group, collide_rect
 
 class Group(pygame_Group):
     def draw(self, onto, offset=(0,0)):
@@ -6,3 +7,18 @@ class Group(pygame_Group):
             sprite.draw()
         # Group.draw() basically just does [onto.blit(s.image, s.rect) for s...]
         super(Group, self).draw(onto)
+
+    def collisions(self):
+        for sprite, other in combinations(self, 2):
+            if collide_rect(sprite, other):
+                if sprite.prev_x is not None:
+                    sprite.x = sprite.prev_x
+                    sprite.y = sprite.prev_y
+                if other.prev_x is not None:
+                    other.x = other.prev_x
+                    other.y = other.prev_y
+                # TODO - be smarter here. work out midpoint and use that.
+                sprite.speed = 0
+                sprite.haptic = 1
+                other.speed = 0
+                other.haptic = 1
